03ce55fc463ce099acdf7f7792076e7a65a15754
Prianshu5145 <agraharipriyanshu52@gmail.com>
2024-11-07 15:31:04 +0530

    image slider

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -3,15 +3,19 @@ import React, { useState, useEffect } from 'react';
 const ImageSlider = () => {
   // Image arrays for mobile and laptop views
   const mobileImages = [
-    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730237481/WhatsApp_Image_2024-10-30_at_02.58.57_0d98de69_t2salr.jpg', // Mobile image paths
-    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730236995/Designer_9_n1okmq.jpg',
-    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730236998/Designer_3_jef4op.jpg',
+    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730971232/Gemini_Generated_Image_r46f4r46f4r46f4r_um5ze0.jpg', // Mobile image paths
+    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730972055/Join_the_Ride_You_Can_Trust_20241107_150126_0000_khswnj.png',
+    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730970960/Every_step_from_choosing_to_owning_made_simple_and_secure_with_TRUST_N_RI_20241107_143913_0000_fgwd4e.jpg',
+    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730970962/Your_paragraph_text_20241107_144125_0000_ufu28x.png',
+    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730972801/Join_Thousands_on_the_Road_to_Trust_20241107_151547_0000_wrkqar.png',
   ];
 
   const laptopImages = [
-   'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730237481/WhatsApp_Image_2024-10-30_at_02.58.57_0d98de69_t2salr.jpg', // Mobile image paths
-    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730236995/Designer_9_n1okmq.jpg',
-    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730236998/Designer_3_jef4op.jpg',
+    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730971232/Gemini_Generated_Image_r46f4r46f4r46f4r_um5ze0.jpg', // Mobile image paths
+    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730972055/Join_the_Ride_You_Can_Trust_20241107_150126_0000_khswnj.png',
+    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730970960/Every_step_from_choosing_to_owning_made_simple_and_secure_with_TRUST_N_RI_20241107_143913_0000_fgwd4e.jpg',
+    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730970962/Your_paragraph_text_20241107_144125_0000_ufu28x.png',
+    'https://res.cloudinary.com/dztz5ltuq/image/upload/v1730972801/Join_Thousands_on_the_Road_to_Trust_20241107_151547_0000_wrkqar.png',
   ];
 
   // State to hold the current image index
@@ -41,12 +45,12 @@ const ImageSlider = () => {
   const currentImage = isMobile ? mobileImages[currentImageIndex] : laptopImages[currentImageIndex];
 
   return (
-    <div className="relative w-full h-84 md:h-full overflow-hidden rounded-sm">
+    <div className="relative w-full h-84 md:h-65 overflow-hidden rounded-sm">
       {/* Image slider */}
       <img
         src={currentImage}
         alt="Slideshow"
-        className="w-full h-full object-cover transition-all duration-500 ease-in-out"
+        className="w-full h-full object-cover transition-all duration-400 ease-in-out"
       />
 
       {/* Optional: Pagination indicators (dots) */}
@@ -54,7 +58,7 @@ const ImageSlider = () => {
         {(isMobile ? mobileImages : laptopImages).map((_, index) => (
           <span
             key={index}
-            className={`w-3 h-3 rounded-full ${currentImageIndex === index ? 'bg-white' : 'bg-gray-400'}`}
+            className={`w-1 h-1 rounded-full ${currentImageIndex === index ? 'bg-white' : 'bg-gray-400'}`}
           />
         ))}
       </div>
